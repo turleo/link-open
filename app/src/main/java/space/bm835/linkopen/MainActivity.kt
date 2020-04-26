76c41980cb86d38b75c06d70f1de12a821ed720a
BM835 <leonid@national.shitposting.agency>
2020-04-26 13:17:15 +1000

    Link unshort feature added

--- app/src/main/java/space/bm835/linkopen/MainActivity.kt
+++ app/src/main/java/space/bm835/linkopen/MainActivity.kt
@@ -1,14 +1,23 @@
 package space.bm835.linkopen
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.net.URLConnection
+
 
 class MainActivity : AppCompatActivity() {
+    var progress: ProgressBar? = null
+    var urlText: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +27,9 @@ class MainActivity : AppCompatActivity() {
 
         val share: Button = findViewById(R.id.button3)
         val open: Button = findViewById(R.id.button)
+        val unshort: Button = findViewById(R.id.unshortButton)
+        progress = findViewById(R.id.loadingUnShort)
+        urlText = findViewById(R.id.textView2)
 
         var url = ""
         if (uri != null) {
@@ -27,17 +39,55 @@ class MainActivity : AppCompatActivity() {
         val out = findViewById<TextView>(R.id.textView2)
         out.text = url
 
-        share.setOnClickListener(View.OnClickListener {
+        share.setOnClickListener {
             val intent = Intent()
             intent.action = Intent.ACTION_SEND
             intent.putExtra(Intent.EXTRA_TEXT, url)
             intent.type = "text/plain"
             startActivity(intent)
-        })
+        }
 
-        open.setOnClickListener(View.OnClickListener {
+        open.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(browserIntent)
-        })
+        }
+
+        unshort.setOnClickListener {
+            val task = RequestTask()
+            task.execute(RequestTaskParams(url, this))
+        }
+    }
+
+    class RequestTaskParams internal constructor(
+        var url: String,
+        var activity: Activity?
+    )
+
+    internal class RequestTask :
+        AsyncTask<RequestTaskParams?, Void?, RequestTaskParams?>() {
+
+
+        override fun onPostExecute(result: RequestTaskParams?) {
+            super.onPostExecute(result)
+            result?.activity?.findViewById<TextView>(R.id.textView2)?.text = result?.url
+        }
+
+        override fun doInBackground(vararg params: RequestTaskParams?): RequestTaskParams? {
+            var uri = params[0]?.url
+            try {
+                val ipAddress = URL(uri)
+                val urlConnection: URLConnection = ipAddress.openConnection()
+                val bufferedReader = BufferedReader(
+                    InputStreamReader(
+                        urlConnection.getInputStream()
+                    )
+                )
+                uri = urlConnection.url.toString()
+                bufferedReader.close()
+            } catch (e: Exception) {
+                return RequestTaskParams(e.localizedMessage, params[0]?.activity)
+            }
+            return RequestTaskParams(uri, params[0]?.activity)
+        }
     }
 }
